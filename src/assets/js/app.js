f48de1be5237f9de6a6aa650be3e916cf1dd4ec2
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-22 21:35:00 +0100

    JavaScript terminado

---
+++ src/assets/js/app.js
@@ -0,0 +1,47 @@
+function mostrarDatosRecogidos() {
+    const name = document.getElementById("name");
+    const nombre = name.value;
+    console.log(`Name: ${nombre}`);
+
+    const email = document.getElementById("email");
+    const correo = email.value;
+    console.log(`Email: ${correo}`);
+
+    const age = document.getElementById("age");
+    const edad = age.value;
+    console.log(`Age: ${edad}`);
+
+    const describeYou = document.getElementById("describe-you");
+    const descripcion = describeYou.value;
+    console.log(`Describe you: ${descripcion}`);
+    
+    const recommend = document.querySelector('input[type=radio][name=recommend]:checked');
+    let recomendado ="";
+    if(recommend!==null) {
+        recomendado = recommend.value;
+    }
+    console.log(`Recommend: ${recomendado}`);
+
+/*
+    const language = document.getElementsByName("language");
+    let elementos = "";
+    language.forEach(elemento => {
+        console.log(elemento);
+        if(elemento.checked){
+            elementos = elemento.value; 
+        }       
+    });
+    console.log (elementos);
+*/
+    const languages = document.querySelectorAll('input:checked');
+    let lenguajes = "";
+    let contador = 1;
+    languages.forEach(elemento => {
+        lenguajes = lenguajes + elemento.value; 
+        if(contador < languages.length) {
+            lenguajes = lenguajes + ", ";
+        }
+        contador++;
+    });
+    console.log(`Lenguajes: ${lenguajes}`);
+}
